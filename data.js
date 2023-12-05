2fcf87c707911a868d17a6ccf218e9dd5887674b
Boi Zak <bruhbruhcast@gmail.com>
2023-12-05 16:39:26 +1100

    gitgitgit

---
+++ data.js
@@ -0,0 +1,112 @@
+var template = {
+    name: "name",
+    fullName: "ifApplicable",
+    status: "deadOrAlive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var quintus = {
+    name: "Quintus",
+    fullName: "Quintus Caecilius Iucundus",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var salvius = {
+    name: "Salvius",
+    fullName: "Gaius Salvius Liberalis",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var varica = {
+    name: "Varica",
+    fullName: "Varica",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var bregans = {
+    name: "Bregans",
+    fullName: "Bregans",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var cogidubnus = {
+    name: "Cogidubnus",
+    fullName: "Tiberius Claudius Cogidubnus",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var dumnorix = {
+    name: "Dumnorix",
+    fullName: "N/A",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var belimicus = {
+    name: "Belimicus",
+    fullName: "N/A",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var clemens = {
+    name: "Clemens",
+    fullName: "Quintus Caecilius Clemens",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var barbillus = {
+    name: "Barbillus",
+    fullName: "N/A",
+    status: "Dead",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var aristo = {
+    name: "Aristo",
+    fullName: "N/A",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var eutychus = {
+    name: "Eutychus",
+    fullName: "N/A",
+    status: "deadOrAlive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var astrologer = {
+    name: "N/A",
+    fullName: "N/A",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
+var petro = {
+    name: "Petro",
+    fullName: "N/A",
+    status: "Alive",
+    description: "physicalDesc",
+    personality: "ifTheyreAnAssholeOrNot",
+    comment: "GrumioReacts",
+}
---
+++ functions.js
@@ -0,0 +1,113 @@
+var current = 0
+
+function getFile(x) {
+    current = x
+}
+
+function updateTable() {
+    if (current == 1) {
+        document.getElementById("name").innerHTML = quintus.name
+        document.getElementById("fullname").innerHTML = quintus.fullName
+        document.getElementById("status").innerHTML = quintus.status
+        document.getElementById("phys").innerHTML = quintus.description
+        document.getElementById("emo").innerHTML = quintus.personality
+        document.getElementById("grumio").innerHTML = quintus.comment 
+    }
+    if (current == 2) {
+        document.getElementById("name").innerHTML = salvius.name
+        document.getElementById("fullname").innerHTML = salvius.fullName
+        document.getElementById("status").innerHTML = salvius.status
+        document.getElementById("phys").innerHTML = salvius.description
+        document.getElementById("emo").innerHTML = salvius.personality
+        document.getElementById("grumio").innerHTML = salvius.comment 
+    }
+    if (current == 3) {
+        document.getElementById("name").innerHTML = varica.name
+        document.getElementById("fullname").innerHTML = varica.fullName
+        document.getElementById("status").innerHTML = varica.status
+        document.getElementById("phys").innerHTML = varica.description
+        document.getElementById("emo").innerHTML = varica.personality
+        document.getElementById("grumio").innerHTML = varica.comment 
+    }
+    if (current == 4) {
+        document.getElementById("name").innerHTML = bregans.name
+        document.getElementById("fullname").innerHTML = bregans.fullName
+        document.getElementById("status").innerHTML = bregans.status
+        document.getElementById("phys").innerHTML = bregans.description
+        document.getElementById("emo").innerHTML = bregans.personality
+        document.getElementById("grumio").innerHTML = bregans.comment 
+    }
+    if (current == 5) {
+        document.getElementById("name").innerHTML = cogidubnus.name
+        document.getElementById("fullname").innerHTML = cogidubnus.fullName
+        document.getElementById("status").innerHTML = cogidubnus.status
+        document.getElementById("phys").innerHTML = cogidubnus.description
+        document.getElementById("emo").innerHTML = cogidubnus.personality
+        document.getElementById("grumio").innerHTML = cogidubnus.comment 
+    }
+    if (current == 6) {
+        document.getElementById("name").innerHTML = dumnorix.name
+        document.getElementById("fullname").innerHTML = dumnorix.fullName
+        document.getElementById("status").innerHTML = dumnorix.status
+        document.getElementById("phys").innerHTML = dumnorix.description
+        document.getElementById("emo").innerHTML = dumnorix.personality
+        document.getElementById("grumio").innerHTML = dumnorix.comment 
+    }
+    if (current == 7) {
+        document.getElementById("name").innerHTML = belimicus.name
+        document.getElementById("fullname").innerHTML = belimicus.fullName
+        document.getElementById("status").innerHTML = belimicus.status
+        document.getElementById("phys").innerHTML = belimicus.description
+        document.getElementById("emo").innerHTML = belimicus.personality
+        document.getElementById("grumio").innerHTML = belimicus.comment 
+    }
+    if (current == 8) {
+        document.getElementById("name").innerHTML = clemens.name
+        document.getElementById("fullname").innerHTML = clemens.fullName
+        document.getElementById("status").innerHTML = clemens.status
+        document.getElementById("phys").innerHTML = clemens.description
+        document.getElementById("emo").innerHTML = clemens.personality
+        document.getElementById("grumio").innerHTML = clemens.comment 
+    }
+    if (current == 9) {
+        document.getElementById("name").innerHTML = barbillus.name
+        document.getElementById("fullname").innerHTML = barbillus.fullName
+        document.getElementById("status").innerHTML = barbillus.status
+        document.getElementById("phys").innerHTML = barbillus.description
+        document.getElementById("emo").innerHTML = barbillus.personality
+        document.getElementById("grumio").innerHTML = barbillus.comment 
+    }
+    if (current == 10) {
+        document.getElementById("name").innerHTML = aristo.name
+        document.getElementById("fullname").innerHTML = aristo.fullName
+        document.getElementById("status").innerHTML = aristo.status
+        document.getElementById("phys").innerHTML = aristo.description
+        document.getElementById("emo").innerHTML = aristo.personality
+        document.getElementById("grumio").innerHTML = aristo.comment 
+    }
+    if (current == 11) {
+        document.getElementById("name").innerHTML = eutychus.name
+        document.getElementById("fullname").innerHTML = eutychus.fullName
+        document.getElementById("status").innerHTML = eutychus.status
+        document.getElementById("phys").innerHTML = eutychus.description
+        document.getElementById("emo").innerHTML = eutychus.personality
+        document.getElementById("grumio").innerHTML = eutychus.comment 
+    }
+    if (current == 12) {
+        document.getElementById("name").innerHTML = astrologer.name
+        document.getElementById("fullname").innerHTML = astrologer.fullName
+        document.getElementById("status").innerHTML = astrologer.status
+        document.getElementById("phys").innerHTML = astrologer.description
+        document.getElementById("emo").innerHTML = astrologer.personality
+        document.getElementById("grumio").innerHTML = astrologer.comment 
+    }
+    if (current == 13) {
+        document.getElementById("name").innerHTML = petro.name
+        document.getElementById("fullname").innerHTML = petro.fullName
+        document.getElementById("status").innerHTML = petro.status
+        document.getElementById("phys").innerHTML = petro.description
+        document.getElementById("emo").innerHTML = petro.personality
+        document.getElementById("grumio").innerHTML = petro.comment 
+    }
+}
+
